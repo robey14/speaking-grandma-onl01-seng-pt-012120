1edc91cb46c74af661ca5352ab9c305ce5907d51
John Michael Robey <robey14@gmail.com>
2020-01-26 00:21:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
 
-if phrase == "SPEAK_TO_GRANDMA"
+if phrase == "speak_to_grandma.upcase"
   puts "NO, NOT SINCE 1938!"
 elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
