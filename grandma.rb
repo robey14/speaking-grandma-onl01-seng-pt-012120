d15ee818758e7447563ec0e7a4a6d08f6f7e303a
John Michael Robey <robey14@gmail.com>
2020-01-26 00:40:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,1 @@
 
-if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
-
-else
-  puts "HUH?! SPEAK UP, SONNY!"
-end
-end
